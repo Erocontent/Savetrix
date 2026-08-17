05a9f9ee5e175b7f21003e18f5f40c7cf7d64694
Erocontent <erocontent@outlook.com>
2026-08-17 08:31:34 -0300

    Add V2.3 configurable hotkeys and selective import controls

--- Savetrix_V2/src/Paths.h
+++ Savetrix_V2/src/Paths.h
@@ -7,4 +7,6 @@ namespace Savetrix::Paths
     [[nodiscard]] std::filesystem::path SkyrimRoot();
     [[nodiscard]] std::filesystem::path ProfileDirectory();
     [[nodiscard]] std::filesystem::path ProfilePath();
+    [[nodiscard]] std::filesystem::path McmDefaultsPath();
+    [[nodiscard]] std::filesystem::path McmUserSettingsPath();
 }
